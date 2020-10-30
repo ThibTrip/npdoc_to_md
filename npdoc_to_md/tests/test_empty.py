da6a43892c1d68d4c23cf12548075abaac9d9daa
Thibault Bétrémieux <40694343+ThibTrip@users.noreply.github.com>
2020-10-30 10:00:48 +0100

    tests: add test for an empty function

---
+++ npdoc_to_md/tests/test_empty.py
@@ -0,0 +1,8 @@
+import os
+from npdoc_to_md import render_md_from_obj_docstring
+
+
+# # Test a function with an empty docstring
+
+def test_empty_func():
+    assert render_md_from_obj_docstring(obj=lambda x: x, obj_namespace='test') == '**<span style="color:purple">test</span>_(x)_**'
